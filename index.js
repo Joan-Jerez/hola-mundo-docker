ce0d4d995ecdd8da9298172b9c67a2fa7b2f64e1
Joan-Jerez <johanjerez52@gmail.com>
2026-04-25 21:35:20 -0400

    fix workflow duplication

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const path = require('path');
-const app = express();
-const port = 3000;
 
-// Servir archivos estáticos desde la carpeta public
+const app = express();
+
+const port = process.env.PORT || 3000;
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Ruta principal
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`App corriendo en http://localhost:${port}`);
+  console.log(`App corriendo en puerto ${port}`);
 });
